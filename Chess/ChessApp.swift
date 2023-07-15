51547038da4464bac12ea5e226f03513542db07a
phuongdoan13 <mphuongdoant@gmail.com>
2023-07-15 22:52:19 +1000

    Initial Commit

---
+++ Chess/ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessApp.swift
+//  Chess
+//
+//  Created by Harry on 15/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
