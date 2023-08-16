0e88799ea22ca4f6142f438ec4cac11760d5234f
phuongdoan13 <mphuongdoant@gmail.com>
2023-08-16 23:55:43 +1000

    Added:checkIfInputIsStandardized()

--- src/Parser.ts
+++ src/Parser.ts
@@ -1,4 +1,27 @@
 
 class Parser{
-  
+  private static instance: Parser;
+
+  private constructor(){}
+
+  public static getInstance(): Parser {
+    if (!Parser.instance) {
+        Parser.instance = new Parser();
+    }
+    return Parser.instance;
+  }
+
+  private promptMove(currentPlayer: string ): string{
+    var move = prompt('Player ' + currentPlayer + ' please input your move: ');
+    return "Thank you for inputting your move";
+  }
+
+  private checkIfInputIsStandardized(move: string): boolean{
+    if(move.length <= 2){
+      return false;
+    }
+    const regexPawnMove = new RegExp("^[a-h][1-8]$");
+    const regexOthersMove = new RegExp("^[KQRBN][a-h][a-h][1-8]$");
+    return regexPawnMove.test(move) || regexOthersMove.test(move);
+  }
 }
